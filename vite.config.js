acc595b809acdc979881a84d172c70e4adc9e046
Yuriine <taigaluigi@gmail.com>
2023-06-02 01:23:38 -0500

    pkpok

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //put your project name here for github pages my projecct name is portfolio
-  base: '/portafolio-yurine/',
+  base: '/portfolio/',
   
 
 
