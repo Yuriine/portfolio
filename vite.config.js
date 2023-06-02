a1902b9e52ea42441ee0fa543997dd286faea9e3
Yuriine <taigaluigi@gmail.com>
2023-06-02 01:16:31 -0500

    first commit 1

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  //put your project name here for github pages my projecct name is portfolio
+  base: '/portafolio-yurine/',
+  
+
+
 });
+
