77d30d49f340eca3ac643e253ea8c9224bd71873
Yuriine <taigaluigi@gmail.com>
2023-07-25 19:52:20 -0500

    cambio

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //put your project name here for github pages my projecct name is portfolio
-  base: '/portfolio/',
-  
-
-
+  base: "/portfolio/",
 });
-
